c86fbc83c40dbcf06a5f2cf21ad05d6bf162c814
SchrodingersFish <SchrodingersFish@outlook.com>
2025-07-08 17:28:52 +0800

    Reduce number of workers in uvicorn run configuration from 18 to 4

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ async def root():
 
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", workers=18)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_level="info", workers=4)
